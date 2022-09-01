7877b8dd7a396a22bfafa7012b35d9c90d9e8a59
ADMIN <ojogunemmy@gmail.com>
2022-09-01 19:00:47 +0100

    safe driving system

--- src/pack/Control.java
+++ src/pack/Control.java
@@ -1,14 +1,26 @@
 package pack;
 
+import java.util.LinkedList;
+
 public class Control {
 
 
+    private LinkedList<Double> doc=new LinkedList<Double>();
     /*
      * automating the process to know when to slow down
      * using automation principles
      */
-    public Control(double speed){
+    public Control(){
 
+    }
+    public void add(double speed){
+        doc.add(speed);
+    }
+
+
+    protected int getSize(){
+
+        return doc.size();
     }
 
 
